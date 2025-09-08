da2159b461e409b939064e4e0415eb3d598e1469
jirehhuang <jirehhuang@ucla.edu>
2025-09-07 22:30:48 -0700

    test: remove fixtures for API URL and key for security

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,18 +11,6 @@ from jhutils.mealie import Mealie
 load_dotenv(Path(__file__).resolve().parents[1] / ".env")
 
 
-@pytest.fixture(name="mealie_api_url", scope="session")
-def fixture_mealie_api_url():
-    """Return Mealie API URL."""
-    return os.getenv("MEALIE_API_URL", "")
-
-
-@pytest.fixture(name="mealie_api_key", scope="session")
-def fixture_mealie_api_key():
-    """Return Mealie API key."""
-    return os.getenv("MEALIE_API_KEY", "")
-
-
 @pytest.fixture(name="mealie_shopping_list_id", scope="session")
 def fixture_mealie_shopping_list_id():
     """Return Mealie shopping list ID."""
@@ -30,9 +18,9 @@ def fixture_mealie_shopping_list_id():
 
 
 @pytest.fixture(name="mealie", scope="function")
-def fixture_mealie(mealie_api_url, mealie_api_key):
+def fixture_mealie():
     """Return Mealie client object."""
     return Mealie(
-        api_url=mealie_api_url,
-        api_key=mealie_api_key,
+        api_url=os.getenv("MEALIE_API_URL", ""),
+        api_key=os.getenv("MEALIE_API_KEY", ""),
     )
