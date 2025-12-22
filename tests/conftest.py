301e728abd37b260c28eb13c9bacd0115b1fefd7
jirehhuang <jirehhuang@ucla.edu>
2025-12-21 21:07:41 -0800

    refactor(test): use .env.test instead of .env when available

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,15 @@ from jhutils import Mealie, Obsidian
 from jhutils.agent._assistant import make_openai_client_from_environ
 from jhutils.agent.tools import Toolset
 
-load_dotenv(Path(__file__).resolve().parents[1] / ".env")
+env_path = Path(__file__).resolve().parents[1]
+dotenv_file = (
+    env_path / ".env.test"
+    if (env_path / ".env.test").exists()
+    else env_path / ".env"
+)
+load_dotenv(dotenv_file, override=True)
 
-TEST_RECIPE_NAME = "Al Pastor"
+TEST_RECIPE_NAME = "Complex Recipe"
 
 
 @pytest.fixture(name="mealie_shopping_list_id", scope="session")
